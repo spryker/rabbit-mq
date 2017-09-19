351c1094970bc219f3a97488fee3899899f928d1
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2017-09-19 08:48:28 +0000

    CORE-1337 fix conflicts

--- src/Spryker/Client/RabbitMq/RabbitMqConfig.php
+++ src/Spryker/Client/RabbitMq/RabbitMqConfig.php
@@ -6,6 +6,8 @@
 
 namespace Spryker\Client\RabbitMq;
 
+use Generated\Shared\Transfer\QueueConnectionTransfer;
+use Generated\Shared\Transfer\RabbitMqOptionTransfer;
 use Spryker\Client\Kernel\AbstractBundleConfig;
 use Spryker\Shared\RabbitMq\RabbitMqConstants;
 
@@ -13,9 +15,39 @@ class RabbitMqConfig extends AbstractBundleConfig
 {
 
     /**
+     * @return \Generated\Shared\Transfer\QueueConnectionTransfer
+     */
+    public function getQueueConnection()
+    {
+        $queueConfig = $this->getQueueConnectionConfig();
+
+        $connectionTransfer = new QueueConnectionTransfer();
+        $connectionTransfer->setHost($queueConfig['host']);
+        $connectionTransfer->setPort($queueConfig['port']);
+        $connectionTransfer->setUsername($queueConfig['username']);
+        $connectionTransfer->setPassword($queueConfig['password']);
+        $connectionTransfer->setVirtualHost($queueConfig['virtualHost']);
+
+        $connectionTransfer->setQueueOptionCollection($this->getQueueOptions());
+
+        return $connectionTransfer;
+    }
+
+    /**
+     * @return \ArrayObject
+     */
+    protected function getQueueOptions()
+    {
+        $queueOptionCollection = new \ArrayObject();
+        $queueOptionCollection->append(new RabbitMqOptionTransfer());
+
+        return $queueOptionCollection;
+    }
+
+    /**
      * @return array
      */
-    public function getQueueConnectionConfig()
+    protected function getQueueConnectionConfig()
     {
         return [
             'host' => $this->get(RabbitMqConstants::RABBITMQ_HOST),
