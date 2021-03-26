4f716b3119e4ef3dfa59ab32be4759ac48d3159b
Dmytro Dymarchuk <dmytro.dymarchuk@spryker.com>
2021-03-26 08:37:47 +0200

    TE-8621: Disable auto-create queue feature - fix phpstan validation

--- src/Spryker/Zed/RabbitMq/Communication/Console/QueueSetupConsole.php
+++ src/Spryker/Zed/RabbitMq/Communication/Console/QueueSetupConsole.php
@@ -31,6 +31,8 @@ class QueueSetupConsole extends Console
     /**
      * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
